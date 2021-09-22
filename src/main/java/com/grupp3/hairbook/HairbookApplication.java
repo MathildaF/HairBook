0d47b9ccc66eb3c0542895902e3141a2fdcdcde8
Mathilda Fritzell <mathilda.fritzell@gmail.com>
2021-09-23 00:16:54 +0200

    test-push

--- src/main/java/com/grupp3/hairbook/HairbookApplication.java
+++ src/main/java/com/grupp3/hairbook/HairbookApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HairbookApplication {
-
+ // test med github
 	public static void main(String[] args) {
 		SpringApplication.run(HairbookApplication.class, args);
 	}
