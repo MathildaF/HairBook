645af7086e4f8a6655f3a1b871ab0df766975cca
Mathilda Fritzell <mathilda.fritzell@gmail.com>
2021-09-23 00:00:07 +0200

    spring-template skapad

---
+++ src/test/java/com/grupp3/hairbook/HairbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp3.hairbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
