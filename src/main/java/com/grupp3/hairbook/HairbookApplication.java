096bd108afe1b1d5299c351c50a52b9108c9ce68
Mona Karlsson <mona.m.karlsson@gmail.com>
2021-09-23 13:21:40 +0200

    test av olika mapping

--- src/main/java/com/grupp3/hairbook/HairbookApplication.java
+++ src/main/java/com/grupp3/hairbook/HairbookApplication.java
@@ -5,14 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HairbookApplication {
- // test med github
 
-	// test 2
-
-	//test 4
-
-	// test 7
 	public static void main(String[] args) {
+
 		SpringApplication.run(HairbookApplication.class, args);
 	}
 
