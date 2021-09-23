60fb930ccf772e96fb2faf1d9f94369aab486412
Chizard <engmantobias@gmail.com>
2021-09-23 09:30:20 +0200

    Test7
    hejehje

--- src/main/java/com/grupp3/hairbook/HairbookApplication.java
+++ src/main/java/com/grupp3/hairbook/HairbookApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HairbookApplication {
  // test med github
 
-	// test 2 
+	// test 2
+	// test 7
 	public static void main(String[] args) {
 		SpringApplication.run(HairbookApplication.class, args);
 	}
